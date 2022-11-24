6e7990f788f88b5805df17e9b001e891bdf7007a
DiasErick <116904851+DiasErick@users.noreply.github.com>
2022-11-23 23:58:10 -0400

    Refactoring

--- app_crawler/cbc.py
+++ app_crawler/cbc.py
@@ -1,51 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getCbcInfoNews():
-    URL = "https://www.cbc.ca/news"
-    resp = requests.get(URL)
-    if resp.status_code == 200:     
-        soup = BeautifulSoup(resp.text, "lxml")        
-        elements = soup.select('[class^="headline"]')
+def getCbcInfo(url: str, element: str):
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.text, "lxml")
+        elements = soup.select(element)
         return elements[0].text
+    return ""
+
+def getCbcInfoNews():
+    return getCbcInfo("https://www.cbc.ca/news", '[class^="headline"]')
     
 def getCbcInfoSports():
-    URL = "https://www.cbc.ca/sports"
-    resp = requests.get(URL)
-    if resp.status_code == 200:     
-        soup = BeautifulSoup(resp.text, "lxml")        
-        elements = soup.select('[class^="headline"]')
-        return elements[0].text
+    return getCbcInfo("https://www.cbc.ca/sports", '[class^="headline"]')
     
 def getCbcInfoRadio():
-    URL = "https://www.cbc.ca/radio"
-    resp = requests.get(URL)
-    if resp.status_code == 200:     
-        soup = BeautifulSoup(resp.text, "lxml")        
-        elements = soup.select('[class^="headline"]')
-        return elements[0].text    
+    return getCbcInfo("https://www.cbc.ca/radio", '[class^="headline"]')
     
 def getCbcInfoMusic():
-    URL = "https://www.cbc.ca/music"
-    resp = requests.get(URL)
-    if resp.status_code == 200:     
-        soup = BeautifulSoup(resp.text, "lxml")        
-        elements = soup.select('[class^="headline"]')
-        return elements[0].text
+    return getCbcInfo("https://www.cbc.ca/music", '[class^="headline"]')
     
 def getCbcInfoTelevision():
-    URL = "https://www.cbc.ca/television"
-    resp = requests.get(URL)
-    if resp.status_code == 200:     
-        soup = BeautifulSoup(resp.text, "lxml")        
-        elements = soup.select('[class^="headline"]')
-        return elements[0].text
+    return getCbcInfo("https://www.cbc.ca/television", '[class^="headline"]')
     
-def getCbcInfo():
-    headline_cbc_news = getCbcInfoNews()
-    headline_cbc_sports = getCbcInfoSports()
-    headline_cbc_radio = getCbcInfoRadio()
-    headline_cbc_music = getCbcInfoMusic()
-    headline_cbc_tv = getCbcInfoTelevision()
+
     
-    return [headline_cbc_news, headline_cbc_sports, headline_cbc_radio, headline_cbc_music, headline_cbc_tv]
